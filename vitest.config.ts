0fc75c6b212dddd3ca7971fa1eee5538f3c2d5a1
Hiroki Hamada <a25135hh@aiit.ac.jp>
2026-01-25 16:04:49 +0900

    feat: envファイルを読み込むように設定

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,26 +2,32 @@
  * Vitest configuration for React components using the jsdom environment.
  */
 import { defineConfig } from "vitest/config";
+import { loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    exclude: [
-      "**/node_modules/**",
-      "**/dist/**",
-      "**/.{idea,git,cache,output,temp}/**",
-      "**/{e2e,playwright}/**",
-    ],
-    reporters: process.env.GITHUB_ACTIONS ? ["dot", "github-actions"] : ["dot"],
-    coverage: {
-      enabled: true,
-      reporter: ["text-summary", "json-summary", "json"],
-      reportOnFailure: true,
-      exclude: ["**/*.d.ts", "**/index.ts", "**/main.tsx", "**/vite.config.*"],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    test: {
+      globals: true,
+      environment: "jsdom",
+      exclude: [
+        "**/node_modules/**",
+        "**/dist/**",
+        "**/.{idea,git,cache,output,temp}/**",
+        "**/{e2e,playwright}/**",
+      ],
+      reporters: process.env.GITHUB_ACTIONS ? ["dot", "github-actions"] : ["dot"],
+      coverage: {
+        enabled: true,
+        reporter: ["text-summary", "json-summary", "json"],
+        reportOnFailure: true,
+        exclude: ["**/*.d.ts", "**/index.ts", "**/main.tsx", "**/vite.config.*"],
+      },
+      env,
     },
-  },
+  };
 });
